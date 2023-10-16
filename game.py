1cf367993298159b61fc2612a8575fc6c577a406
Rehloletswe Motlatla <148054835+rehloletsweDEF@users.noreply.github.com>
2023-10-16 15:07:47 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
+import random
+import time
 
+secret_number = random.randint(1,100)
+
+attempts = 0
+maximum attempts = 2
+score = 0
+leaderboard = [] 
+
+#Create a time function
+
+def timer(start_time)
+
+#Display Menu
+
+print("User menu")
+print("Pick number")
+print("Settings")
+print("Profile")
+print("Exit")
+
+print("2. Profile")
